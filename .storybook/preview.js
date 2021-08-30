902113fdba861bfab586bd36883cd7d3d8556b05
Edward Benson <edward.benson@gmail.com>
2021-08-30 17:24:47 -0400

    Tailwind and PostCSS are mostly configured

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import Layout from './Layout';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,23 @@ export const parameters = {
     },
   },
 }
+
+addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
+addDecorator(withInfo({
+  inline: true,
+  styles: (base) => ({
+    ...base,
+    infoBody: {
+      ...base.infoBody,
+    },
+    source: {
+      h1: {
+        margin: '20px 0',
+        padding: '0 0 5px 0',
+        fontSize: '25px',
+        borderBottom: '1px solid #EEE',
+      },
+    },
+  }),
+  propTablesExclude: [Layout]
+}));
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Thank you!
 
-This repository was configured via the awesome blog writeup of Prateek Surana at https://prateeksurana.me/blog/react-component-library-using-storybook-6/.
+This repository was configured via these awesome resourceds:
+
+* Prateek Surana's Blog writeup at https://prateeksurana.me/blog/react-component-library-using-storybook-6/.
+* https://dev.to/elisealcala/start-a-component-library-with-storybook-tailwind-and-typescript-2ofa
+
